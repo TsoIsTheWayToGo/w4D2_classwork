ee0be19f87bf67ca8bd1a2793ebcfc1519ba47a0
TsoIsTheWayToGO <ericgordontso@gmail.com>
2019-01-29 15:54:57 -0800

    Add views

--- cats99/app/controllers/cats_controller.rb
+++ cats99/app/controllers/cats_controller.rb
@@ -1,11 +1,27 @@
 class CatsController < ApplicationController
   def index
-    cat = Cat.all
+    @cats = Cat.all
   end
 
   def show
-    cat = Cat.find(params[:id])
-    render json: cat 
+    @cat = Cat.find(params[:id])
   end
   
+  def new
+    @cat = Cat.new
+
+  end
+  def create
+    @cat = Cat.new(cat_params)
+    
+    if @cat.save
+      redirect_to cats_url
+
+    end
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name,:birth_date,:color,:description,:sex)
+  end
 end
